b61d65f9263910011ad2ef1c7998f9e931275ca9
Illia Popov <neoflaminis@gmail.com>
2022-11-17 03:28:52 +0200

    initial

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Point:
-    x: int
-    y: int
+    x: float
+    y: float
 
 
 @dataclass
 class EllipticCurve:
-    pass
+    a: float
+    b: float
 
 
 class ECPoint:
@@ -18,56 +20,57 @@ class ECPoint:
         pass
 
     @staticmethod
-    def ECPointGen(x: int, y: int) -> Point:
+    def ECPointGen(x: float, y: float) -> Point:
         return Point(x, y)
-    
+
     @staticmethod
-    def IsOnCurveCheck(a: Point) -> bool:
-        pass
-    
+    def IsOnCurveCheck(a: Point, curve: EllipticCurve) -> bool:
+        return a.y**2 - a.x**3 - curve.a*a.x - curve.b == 0
+
     @staticmethod
     def AddECPoints(a: Point, b: Point) -> Point:
-        try:
-            m = (a.y - b.y) / (a.x - b.x)
-        except Exception as e:
-            m = 0
+
+        m = (a.y - b.y) / (a.x - b.x)
+
         x_r = m*m - a.x - b.x
         y_r = a.y + m * (x_r - a.x)
 
-        return Point(x_r, y_r)
+        return Point(x_r, -y_r)
 
     @staticmethod
     def DoubleECPoints(point: Point, a: int) -> Point:
         x_r = ((3 * point.x * point.x + a) / (2 * point.y))**2 - 2 * point.x
-        y_r = -point.y + ((3 * point.x * point.x + a) / (2 * point.y)) * (point.x - x_r)
+        y_r = -point.y + ((3 * point.x * point.x + a) /
+                          (2 * point.y)) * (point.x - x_r)
 
         return Point(x_r, y_r)
-        
-    
+
     @staticmethod
-    def ScalarMult(a : Point, k: int) -> Point:
-        pass
+    def ScalarMult(a: Point, k: int, curve: EllipticCurve) -> Point:
+
+        res_point = a
+        i = 1
+        while i < k:
+            if i * 2 > k:
+                res_point = ECPoint.AddECPoints(res_point, a)
+                i += 1
+            else:
+                res_point = ECPoint.DoubleECPoints(res_point, curve.a)
+                i *= 2
+
+        return res_point
 
     @staticmethod
     def ECPointToString(point: Point) -> str:
-        pass
-    
+        return point.__str__()
+
     @staticmethod
-    def PrintECPoin(point: Point) -> str:
+    def PrintECPoint(point: Point) -> None:
         print(ECPoint.ECPointToString(point))
 
 
-print(ECPoint.AddECPoints(Point(1,2), Point(1,2)))
-
-print(ECPoint.DoubleECPoints(Point(1, 2), -7))
-
-
-
-
-
-
-
-
-    
-
-
+curve = EllipticCurve(-7, 10)
+#print(ECPoint.AddECPoints(Point(1,2), Point(1,2)))
+#print(ECPoint.IsOnCurveCheck(Point(4, 5), curve=curve))
+print(ECPoint.ScalarMult(Point(1, 2), 4, curve))
+#print(ECPoint.DoubleECPoints(Point(1, 2), -7))
